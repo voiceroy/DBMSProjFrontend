f09976f621c2aeb7e953eb0bbc6766c394f5e813
Harsh <haryan998@gmail.com>
2024-07-23 18:16:11 +0530

    Added JS.

---
+++ app.js
@@ -0,0 +1,6 @@
+const order = document.getElementById('order-btn');
+
+order.addEventListener("click", () => {
+    console.log("click");
+    window.location.href = 'http://127.0.0.1:5500/#piz';
+})
